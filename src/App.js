c0130516f4e2e83cda3a327bfad0bb50a003e817
Vojta Holik <vojta@egghead.io>
2020-07-13 12:25:11 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Image from "./components/design"
+
+export default function App() {
+  return (
+    <div className="App">
+      Image Design:
+      <Image />
+    </div>
+  )
+}
---
+++ src/components/design.js
@@ -0,0 +1,59 @@
+/** @jsx jsx */
+import {jsx, Global} from "@emotion/core"
+import Textfit from "react-textfit"
+import Twemoji from "react-twemoji"
+
+export default function Design() {
+  return (
+    <div
+      css={{
+        width: 1280,
+        height: 720,
+        background: `rgb(255, 255, 255)`,
+        position: "absolute",
+        display: "flex",
+        overflow: "hidden",
+      }}
+    >
+      <Global
+        styles={{
+          "*": {
+            boxSizing: "border-box",
+            margin: 0,
+            padding: 0,
+            fontFamily: "system-ui",
+          },
+          ".emoji": {
+            height: "1em",
+            width: "1em",
+            margin: "0 .05em 0 .1em",
+            verticalAlign: "-0.1em",
+          },
+        }}
+      />
+
+      <h1
+        css={{
+          color: "red",
+          height: "100%",
+          textAlign: "center",
+        }}
+      >
+        <Textfit
+          max={256}
+          min={24}
+          style={{
+            minHeight: "80%",
+            maxHeight: "80%",
+            lineHeight: 1,
+          }}
+        >
+          sdsd
+          <Twemoji options={{className: "emoji", folder: "svg", ext: ".svg"}}>
+            {window.title}
+          </Twemoji>
+        </Textfit>
+      </h1>
+    </div>
+  )
+}
