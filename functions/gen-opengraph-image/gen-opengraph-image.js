dddbbe2f4092b677d21d2eeca902877e3aabeea5
Vojta Holik <vojta@egghead.io>
2020-07-13 11:55:37 +0200

    hm

--- functions/gen-opengraph-image/gen-opengraph-image.js
+++ functions/gen-opengraph-image/gen-opengraph-image.js
@@ -1,6 +1,6 @@
-const playwright = require('playwright-aws-lambda')
-const fs = require('fs')
-const script = fs.readFileSync('./image.js', 'utf-8')
+const playwright = require("playwright-aws-lambda")
+const fs = require("fs")
+const script = fs.readFileSync("./src/image.js", "utf-8")
 
 exports.handler = async function (event, ctx) {
   const browser = await playwright.launchChromium()
@@ -23,19 +23,19 @@ exports.handler = async function (event, ctx) {
   `)
   const {queryStringParameters} = event
   const tags = queryStringParameters.tags
-    ? decodeURIComponent(queryStringParameters.tags).split(',')
+    ? decodeURIComponent(queryStringParameters.tags).split(",")
     : []
   await page.addScriptTag({
     content: `
-  window.title = "${queryStringParameters.title || 'No Title'}";
+  window.title = "${queryStringParameters.title || "No Title"}";
   window.tags = ${JSON.stringify(tags)};
-  window.author = "${queryStringParameters.author || ''}";
+  window.author = "${queryStringParameters.author || ""}";
   window.image = "https://joelhooks.com/images/logo.png"
   `,
   })
   await page.addScriptTag({content: script})
   const boundingRect = await page.evaluate(() => {
-    const corgi = document.getElementById('corgi')
+    const corgi = document.getElementById("corgi")
     const {x, y, width, height} = corgi.children[0].getBoundingClientRect()
 
     return {x, y, width, height}
@@ -53,9 +53,9 @@ exports.handler = async function (event, ctx) {
     isBase64Encoded: true,
     statusCode: 200,
     headers: {
-      'Content-Type': 'image/png',
-      'Content-Length': screenshotBuffer.length.toString(),
+      "Content-Type": "image/png",
+      "Content-Length": screenshotBuffer.length.toString(),
     },
-    body: screenshotBuffer.toString('base64'),
+    body: screenshotBuffer.toString("base64"),
   }
 }
